2761d58488744b990205bc52edd0a47997ec8e78
aladdin-mck <aladdin320@yahoo.com>
2019-06-14 15:18:30 -0400

    deleted files

--- flower-bot-driver.py
+++ flower-bot-driver.py
@@ -1,4 +1,5 @@
-
+# Boolean for ultrasonic sensor
+ultraSonic = false
 
 # Circumference in cm
 circum = 17.954
@@ -28,9 +29,32 @@ def getAngle():
 def getShift():
     return shift
 
-def angleCorr():
+def angleCorr(angle):
+    '''
+    if angle is less than 90 - dynaX1(time, vel) for angle correction
+    and dynaY1(time, -vel) & dynaY2(time, vel) up y-axis for shift correction
     
-def shiftCorr():
+    if angle is greater than 90 - dynaX2(time, vel) for angle correction
+    and dynaY1(time, vel) & dynaY2(time, -vel) down y-axis for shift correction
+    '''
+    # this still needs some equation that 
+    # takes angle as input and outputs time 
+
+    if (angle < 90):
+        dynaX1(time,vel)
+    if (angle > 90):
+        dynaX2(time,vel)
     
+def shiftCorr(shift):
+    if (shift > 0):
+        dynaY1(time,-vel)
+        dynaY2(time,vel)
+        
+
+def shift_x():
+
+def shift_y():
+
+def angle_dist():
 
 move_y(10)
